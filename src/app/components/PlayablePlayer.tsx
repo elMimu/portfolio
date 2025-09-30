2f22ed1c20cdfa9c0922785f8637cb6d4cc54c5a
Michel Munhoz <mmunhz@gmail.com>
2025-09-30 17:27:35 -0300

    feat(playable_player): can reload and change playable orientation

--- src/app/components/PlayablePlayer.tsx
+++ src/app/components/PlayablePlayer.tsx
@@ -1,23 +1,71 @@
-"use client"
+"use client";
+
 // Iphone SE = {375, 667}
 // S8 = {369, 740}
+// max-size = {350, 667}
+
+import { Playable } from "@/types/playable";
+import playable_list from "@/data/playable_list.json";
 
 import { useState } from "react";
 
-// max-size = {350, 667}
-//
-
-const current_src = "/playables/wm_ataque_cidade/index.html";
+let playable: Playable | null = (() => {
+  if (playable_list.length > 0) {
+    return playable_list[0];
+  }
+  return null;
+})();
 
 export default function PlayablePlayer() {
-  const [source, setSource] = useState(current_src);
+  const [loadCount, setLoadCount] = useState(0);
+  const [{ w, h }, setOrientation] = useState({ w: 350, h: 667 });
+
   return (
     <div>
-      <nav className="border"></nav>
-      <iframe
-        className="mx-auto border w-[350] h-[667]"
-        src={source}
-      ></iframe>
+      <menu className="border">
+        <select
+          className="border"
+          onChange={(ev) => {
+            playable = playable_list[parseInt(ev.target.value)];
+            console.log(playable);
+            setLoadCount(loadCount + 1);
+          }}
+        >
+          {playable_list.map((p, i) => {
+            return (
+              <option key={i} value={i}>
+                {p.name}
+              </option>
+            );
+          })}
+        </select>
+        <span>
+          <button
+            className="border"
+            onClick={() => {
+              setLoadCount(loadCount + 1);
+            }}
+          >
+            Reload
+          </button>
+          <button
+            className="border"
+            onClick={() => {
+              setOrientation((prev) => ({ w: prev.h, h: prev.w }));
+            }}
+          >
+            Orientation
+          </button>
+        </span>
+      </menu>
+      <div>
+        <iframe
+          key={loadCount}
+          className={`mx-auto border`}
+          style={{ width: w, height: h }}
+          src={playable ? playable.src : ""}
+        />
+      </div>
     </div>
   );
 }
---
+++ src/types/playable.ts
@@ -0,0 +1,4 @@
+export type Playable = {
+  name: string;
+  src: string;
+};
