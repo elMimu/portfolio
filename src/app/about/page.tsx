79f699e2522a0b956919067503515a369c915dca
Michel <mmunhz@gmail.com>
2025-10-15 14:50:40 -0300

    feat: add contact image links

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,23 +1,43 @@
 import { textTemplate } from "@/utils/textTemplate";
+import Image from "next/image";
 
 export default function AboutPage() {
   return (
     <div className="flex justify-center w-full mt-10">
-      <div className="relative border w-5xl">
+      <div className="relative w-5xl">
         <div className=" mb-10">
-          <h2 className="text-2xl">About</h2>
-          <p className=" text-justify">{textTemplate}</p>
-        </div>
-        <div className="mb-10">
-          <h2 className="text-2xl text-right text-2xl">
-            As a programmer...
-          </h2>
+          <h2 className="text-2xl header-secondary">About</h2>
+          <p className=" text-justify mb-10">{textTemplate}</p>
           <p className="text-justify">{textTemplate}</p>
         </div>
-        <div className="absolute top-full -translate-y-full w-full">
+        <div className="mb-10">
+          {/* <h2 className="text-2xl text-right text-2xl"> */}
+          {/*   As a programmer... */}
+          {/* </h2> */}
+        </div>
+        <div className="absolute top-full -translate-y-full w-full p-20">
           <hr className="m-1" />
-          <h2 className="text-lg text-center text-2xl">Contact</h2>
-          <div>LINK</div>
+          <h2 className="text-lg text-center text-2xl pb-2">Contact Information</h2>
+          {/* <img src="/github-mark.svg" alt=""/>  */}
+
+          <nav className="flex justify-center gap-2">
+            <a href="#">
+              <Image
+                width={30}
+                height={30}
+                src="/github-mark.svg"
+                alt="GitHub"
+              />
+            </a>
+            <a href="#">
+              <Image
+                width={30}
+                height={30}
+                src="/linkedin-svgrepo-com.svg"
+                alt="GitHub"
+              />
+            </a>
+          </nav>
         </div>
       </div>
     </div>
