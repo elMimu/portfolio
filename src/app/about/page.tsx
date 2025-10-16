ea058e0c86159e2708e2353499658064c3334d48
Michel <mmunhz@gmail.com>
2025-10-16 08:56:03 -0300

    fix: avoid overlapping on about section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,21 +4,19 @@ import Image from "next/image";
 export default function AboutPage() {
   return (
     <div className="flex justify-center w-full mt-10">
-      <div className="relative w-5xl">
-        <div className=" mb-10">
+      <div className="flex flex-col w-5xl">
+        <div className="flex-1 mb-10 p-2">
           <h2 className="text-2xl header-secondary">About</h2>
           <p className=" text-justify mb-10">{textTemplate}</p>
           <p className="text-justify">{textTemplate}</p>
         </div>
-        <div className="mb-10">
-          {/* <h2 className="text-2xl text-right text-2xl"> */}
-          {/*   As a programmer... */}
-          {/* </h2> */}
-        </div>
-        <div className="absolute top-full -translate-y-full w-full p-20">
+        <div className="mb-10"></div>
+
+        <div className="w-full p-20">
           <hr className="m-1" />
-          <h2 className="text-lg text-center text-2xl pb-2">Contact Information</h2>
-          {/* <img src="/github-mark.svg" alt=""/>  */}
+          <h2 className="text-lg text-center text-2xl pb-2">
+            Contact Information
+          </h2>
 
           <nav className="flex justify-center gap-2">
             <a href="#">
